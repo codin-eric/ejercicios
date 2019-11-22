be0e3de30eebfbef41a9244cb312dee3c5d0873f
risheric <risheric@gmail.com>
2019-11-22 10:27:31 -0300

    added conditionals

---
+++ 02_condicionales/ejercicios.py
@@ -0,0 +1,26 @@
+"""
+1. Continuando con el ejercicio del video pedir tu nombre y tu año de
+    nacimiento. Mostrar por consola cuanto años tenes.
+
+    Ingrese su nombre: Eric
+    Ingrese su año de nacimiento: 1991
+    
+    "Hola Eric de 28 años"
+
+    Más adelante veremos como solucionar el problema de si todavia no pasó tu
+    mes de cumpleaños
+
+2. Pedir 2 numeros por consola y realizar las 4 operaciones básicas.
+    Suma, resta, multiplicación y división. Mostrar los resultados por consola
+
+    Ingrese el primer numero: 6
+    Ingrese el segundo numero: 2
+
+    "Suma: 8"
+    "Resta: 4"
+    "Multiplicación: 12"
+    "División: 3"
+
+    Más adelante veremos como resolver el problema cuando se divide por 0
+
+"""
